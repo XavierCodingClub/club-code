abb697497dd359024694b0397b8d6884e41bd98b
Alden Wang <114105823+AldenStripes@users.noreply.github.com>
2024-12-03 13:50:33 -0500

    Create template.py

---
+++ advent-of-code/template.py
@@ -0,0 +1,5 @@
+with open("filename.txt", "r") as file:  # open file in read mode
+    ans = 0
+    for line in file:
+        s = line.strip() # current line without trailing whitespace
+    print(ans) 
