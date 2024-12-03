589f0074a18ff799040038dcc494c331810c7295
Alden Wang <114105823+AldenStripes@users.noreply.github.com>
2024-12-03 13:45:09 -0500

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,25 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-typedef pair<int, int> pi;
-#define all(i) i.begin(), i.end()
-const int inf = 0x3f3f3f3f;
-
 int main() {
-    ios::sync_with_stdio(0); cin.tie(0);
-    fstream File("file.txt");
+    fstream File("file.txt"); // copy paste given input into file called file.txt (or whatever u want to call it)
 
+    int ans = 0;
     string line;
     while (getline(File, line)) {
-        // If you are reading one num
-        /*
-            int num = stoi(line);
-        */
-
         // If you are just reading
         /*
         cout << line << '\n';
+        */
+        
+        // If you are reading one num
+        /*
+            int num = stoi(line);
         */
 
         // If you are reading two space-separated nums
@@ -29,4 +24,5 @@ int main() {
             stream >> x >> y;
         */
     }
+    cout << ans << '\n';
 }
